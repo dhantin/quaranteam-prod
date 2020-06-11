3ce034feffbae21c55287517b92bcb67fa880c5b
Kaan Aksoy <kaanaksoyaz@gmail.com>
2020-06-11 12:46:00 -0700

    Updates

---
+++ precache-manifest.7df244d0efa179305824e725df8a0b49.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0a8c10378a1d5fcbc2d2c699955bbd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3678204c7df97eab7ae",
+    "url": "/static/css/2.b8b3bc4e.chunk.css"
+  },
+  {
+    "revision": "1857939c55a9ae9d480a",
+    "url": "/static/css/main.5425fbdb.chunk.css"
+  },
+  {
+    "revision": "b3678204c7df97eab7ae",
+    "url": "/static/js/2.32da840a.chunk.js"
+  },
+  {
+    "revision": "2acc0ffdca31fa47b43687215a126164",
+    "url": "/static/js/2.32da840a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1857939c55a9ae9d480a",
+    "url": "/static/js/main.eae18f3b.chunk.js"
+  },
+  {
+    "revision": "a499edf442936175b1c0",
+    "url": "/static/js/runtime-main.3cc42834.js"
+  },
+  {
+    "revision": "ebd3b92b922ffa030e0bc8c7286d37ab",
+    "url": "/static/media/Jost-VariableFont_wght.ebd3b92b.ttf"
+  },
+  {
+    "revision": "f540a12d889f1ab9ed9a6cee78a03dac",
+    "url": "/static/media/default-profile.f540a12d.png"
+  },
+  {
+    "revision": "6494ab5b3760b5e2cc46f5566a245371",
+    "url": "/static/media/stars.6494ab5b.png"
+  }
+]);
